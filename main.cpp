49886803bca92110c9eb5a1f7ee0d117a0c28d6e
Austin Ngobeni <kngobeni223@gmail.com>
2025-01-16 20:25:41 +0200

    read the video feed

--- main.cpp
+++ main.cpp
@@ -12,4 +12,20 @@ int main()
         std::cout << "Error opening camera" << std::endl;
         exit(1);
     }
+
+    while (true) {
+        cv::Mat frame;
+        cam >> frame;
+        if (frame.empty()) {
+            std::cerr << "Error reading frame" << std::endl;
+            exit(1);
+        }
+        cv::Mat flipped_output;
+        cv::flip(frame, flipped_output, 1);
+        cv::imshow("Cam", flipped_output);
+
+        char keypress = cv::waitKey(1);
+        if (keypress == 27)
+            break;
+    }
 }
